a6d05c348e475b18c7b904dfd9b73bded4626490
DhanaLakshmi1779659 <81734112+DhanaLakshmi1779659@users.noreply.github.com>
2021-04-17 14:26:49 +0530

    Update JenkinsDemo.java
    commited

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,6 +1,6 @@
 public class JenkinsDemo 
 {
-  public static void main(String args[])
+  public static void main(String Args[])
   {
     System.out.println("this is my first java program for jenkins");
   }
